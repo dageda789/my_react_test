e828897c1b9ad35ddd8cb9fa1fdfaff8eb07aa2c
wensenbiao <nz62qt8@dingtalk.com>
2023-03-22 14:22:36 +0800

    niceform

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NiceTable from './components/NeceTable';
+import NiceTable, { FetchParamsType } from './components/NeceTable';
 // import NiceForm from './components/NiceForm';
 // import DivScroller from './examples/WindowScroller';
 
@@ -7,11 +7,50 @@ import NiceTable from './components/NeceTable';
 
 const App: React.FC = () => {
 
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+    },
+  ];
+  const getDatas = async(params: FetchParamsType) => {
+   let arr = []
+   const {current, pageSize} = params
+
+   for (let i = 0; i < current! * pageSize!; i ++) {
+    arr.push({
+      key: i,
+      name: `Edward King ${i}`,
+      age: `1${i}`,
+      address: `London, Park Lane no. ${i}`,
+    })
+   }
+
+   return {
+    data: arr,
+    totalCount: arr.length
+   }
+
+  }
+
+
+
   return (
     <div>
       {/* <DivScroller /> */}
     测试组件
-    <NiceTable />
+    <NiceTable
+      fetchDataSource={(params) => getDatas(params as  FetchParamsType )}
+      columns={columns}
+      />
     </div>
   );
 };
--- src/components/NeceTable/index.tsx
+++ src/components/NeceTable/index.tsx
@@ -1,60 +1,123 @@
-import { PaginationProps, Table } from 'antd'
-import React from 'react'
-
-const dataSource = [
-  {
-    key: '1',
-    name: '胡彦斌',
-    age: 32,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '2',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-];
+import { Form, Pagination, PaginationProps, Table, TableProps } from "antd";
+import React, { useEffect, useRef, useState } from "react";
 
 
-const columns = [
-  {
-    title: '姓名',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: '住址',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
+export interface FetchParamsType {
+  /**
+   * 当前页
+   */
+  current?: number;
+  /**
+   * 当前页数
+   */
+  pageSize?: number;
+}
 
-  const NiceTable = React.forwardRef(() => {
+interface NiceTableProps<T> extends Omit<TableProps<T>, "pagination"> {
+  fetchDataSource?: (params: FetchParamsType) => Promise<any>;
+  scroll?: { y: "100%" } | { x: "100%" };
+  pagination?: PaginationProps | null;
+  onPaginationChange?: (page: number, size: number) => void;
+}
 
-    const paginationProps: PaginationProps  =  {
-      size: 'small',
+const NiceTable = React.forwardRef(
+  <T extends {}>(props: NiceTableProps<T>, ref: any) => {
+    const {
+      fetchDataSource,
+      onPaginationChange,
+      scroll = { y: "100%" },
+      pagination = {},
+      ...restProps
+    } = props;
+
+    const [loading, setLoading] = useState(false);
+    const [tableData, setTableData] = useState<any>([]);
+    const pageSizeRef = useRef({
+      current: 1,
+      pageSize: 10,
+    });
+
+    /**
+     *  初始化数据
+     * @param params 
+     */
+    const getDataSource = async (params?: FetchParamsType) => {
+      if (fetchDataSource && !("dataSource" in props)) {
+        setLoading(true);
+        const res = await fetchDataSource({
+          ...pageSizeRef.current,
+          ...params,
+        });
+        if (res) {
+          setTableData(res);
+        }
+        setLoading(false);
+      }
+    };
+
+    useEffect(() => {
+      getDataSource();
+    }, []);
+
+
+
+    /**
+     * 分页change
+     * @param page 当前页
+     * @param size 当前分页
+     */
+    const handlePaginationChange = (page: number, size: number) => {
+      if (!("current" in (pagination as FetchParamsType))) {
+        pageSizeRef.current = {
+          ...pageSizeRef.current,
+          current: page,
+        };
+      }
+      if (!("pageSize" in (pagination as FetchParamsType))) {
+        pageSizeRef.current = {
+          ...pageSizeRef.current,
+          pageSize: size,
+        };
+      }
+      if (onPaginationChange) {
+        onPaginationChange?.(page, size);
+      }
+      getDataSource();
+    };
+
+
+    const totalCount = pagination?.total || tableData.totalCount || 0
+    const paginationProps: PaginationProps =  {
+      size: "small",
+      current: pageSizeRef.current.current,
+      pageSize: pageSizeRef.current.pageSize,
       showSizeChanger: true,
       showQuickJumper: true,
-      pageSizeOptions: ['10', '20', '50', '100'],
-      showTotal: () => <></>
-    }
-
+      pageSizeOptions: ["10", "20", "50", "100"],
+      onChange: handlePaginationChange,
+      ...pagination,
+      total: totalCount,
+      showTotal: () => <></>,
+    };
 
     return (
-      <Table
-        dataSource={dataSource}
-        columns={columns}
-        // pagination
-        />
-
-    )
-  })
-
+      <div style={{ width: '70%', height: '50%'}} >
+        <Form></Form>
+        <div>
+          <Table
+            loading={loading}
+            dataSource={tableData.data ?? tableData}
+            {...restProps}
+            pagination={false}
+            scroll={scroll}
+          />
+        </div>
+        <div>
+          <Pagination {...paginationProps} />
+        </div>
+      </div>
+    );
+  }
+);
 
 export default NiceTable;
